be6379d7fd90c216bbcb9d270ac856fecf34a09e
michaelrodgers1983 <82784452+michaelrodgers1983@users.noreply.github.com>
2021-05-31 14:48:51 +1000

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "ewcZ6le,2vCt!/4~wL!p":
+            case "-":return img`
+. . . . . . . c c . . . . . . . 
+. . . . . . c a a c . . . . . . 
+. . . . . . c a a c . . . . . . 
+. . . d . c a d d a c . d . . . 
+. . . d . c a d d a c . d . . . 
+. d . d c a d d d d a c d . d . 
+. d . c a a d b b d a a c . d . 
+. d c a a b b b b b b a a c d . 
+. c a a b b d b b d b b a a c . 
+c a a a a a d b b d a a a a a c 
+c a a a c a a a a a a c a a a c 
+c a a c d c a c c a c d c a a c 
+c a c d 2 d c . . c d 2 d c a c 
+. c 5 4 2 4 5 . . 5 4 2 4 5 c . 
+. . . 5 4 5 . . . . 5 4 5 . . . 
+. . . . 5 . . . . . . 5 . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
