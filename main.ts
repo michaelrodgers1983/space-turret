a67fe18e17c414f32e05117daaca70bd5434d24d
michaelrodgers1983 <82784452+michaelrodgers1983@users.noreply.github.com>
2021-06-01 15:40:59 +1000

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ let Cruiser = sprites.create(assets.image`-`, SpriteKind.Player)
 controller.player1.moveSprite(Cruiser, 70, 0)
 tiles.setTilemap(tilemap`level1 -`)
 tiles.placeOnTile(Cruiser, tiles.getTileLocation(5, 59))
-Cruiser.setVelocity(0, -20)
 forever(function () {
     scene.centerCameraAt(0, Cruiser.y - 45)
+    Cruiser.setVelocity(0, -20)
 })
