d414b4aadc222f361191fd4ba34e5c754d5f1355
michaelrodgers1983 <82784452+michaelrodgers1983@users.noreply.github.com>
2021-06-04 16:11:38 +1000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,35 @@
+controller.player2.onButtonEvent(ControllerButton.Right, ControllerButtonEvent.Pressed, function () {
+    Turret_Angle += -10
+})
+controller.player2.onButtonEvent(ControllerButton.Left, ControllerButtonEvent.Pressed, function () {
+    Turret_Angle += 10
+})
 let Cruiser = sprites.create(assets.image`-`, SpriteKind.Player)
+let Turret = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . c c . . . . . . . 
+    . . . . . . . c c . . . . . . . 
+    . . . . . . . c c . . . . . . . 
+    . . . . . . . c c . . . . . . . 
+    . . . . . . c c c c . . . . . . 
+    . . . . . . c a a c . . . . . . 
+    . . . . . c a b b a c . . . . . 
+    . . . . . c a b b a c . . . . . 
+    . . . . . . c a a c . . . . . . 
+    . . . . . . . c c . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
 controller.player1.moveSprite(Cruiser, 70, 0)
 tiles.setTilemap(tilemap`level1 -`)
 tiles.placeOnTile(Cruiser, tiles.getTileLocation(5, 59))
+let Turret_Angle = 0
 forever(function () {
-    scene.centerCameraAt(0, Cruiser.y - 45)
+    scene.centerCameraAt(0, Cruiser.y - 30)
     Cruiser.setVelocity(0, -20)
+    Turret.setPosition(Cruiser.x, Cruiser.y - -20)
+    transformSprites.rotateSprite(Turret, Turret_Angle)
 })
